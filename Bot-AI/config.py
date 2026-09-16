0b6f2668cb4189bf3bc44ba3c92fa12a7960d86b
Smertnik2 <134734133+Smertnik2@users.noreply.github.com>
2026-09-16 19:24:50 +0200

    Delete Bot-AI directory

--- Bot-AI/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GCP_API_KEY = os.getenv("GCP_API_KEY")  # если используешь
-ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
--- Bot-AI/install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001
-python -m pip install --upgrade pip
-pip install python-telegram-bot google-genai Pillow qrcode python-dotenv
-echo.
-echo [OK] Все библиотеки установлены!
-pause
