d1805e1e2fd83ed7b515937e9f4ec7b9f18d54d4
Smertnik2 <134734133+Smertnik2@users.noreply.github.com>
2026-09-16 19:01:11 +0200

    Update config.py

--- Bot-AI/config.py
+++ Bot-AI/config.py
@@ -4,4 +4,4 @@ load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 GCP_API_KEY = os.getenv("GCP_API_KEY")  # если используешь
-ADMIN_CHAT_ID = os.getenv("ADMIN_ID")
+ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
