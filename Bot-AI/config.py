28d7cd56b7912193cf9eb4cbc8c23f7fd41008de
Smertnik2 <134734133+Smertnik2@users.noreply.github.com>
2026-09-16 18:31:49 +0200

    Update config.py

--- Bot-AI/config.py
+++ Bot-AI/config.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
-TELEGRAM_TOKEN = "8824904784:AAF5VVB8LG32avZopFIJSROfWqVtQ6hC2jc"
-GEMINI_API_KEY = "AQ.Ab8RN6JysDG1-Icb2q-UdSHGMjYcMqRUae8TMJV5MTwmNmGXhA"
-ADMIN_CHAT_ID = -1004450796006
-BOT_USERNAME = "repairfix_help_bot"
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-MODEL = "models/gemini-3.5-flash-lite"
-ADDRESS = "Swiebodzice,ul.Kolejowa 12"
-PHONE_MASTER = "+48 696 175 928"
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GCP_API_KEY = os.getenv("GCP_API_KEY")  # если используешь
+ADMIN_ID = os.getenv("ADMIN_ID")
